72a9a0ac2fb1396242ea07e9c6ad7f88614e969f
Aduan Vieira <aduannv@gmail.com>
2024-10-04 14:50:58 -0300

    feature: improviments in swagger documentation

--- src/123Vendas.Domain/Entities/Sale.cs
+++ src/123Vendas.Domain/Entities/Sale.cs
@@ -8,7 +8,7 @@ public class Sale(Guid customerId, string customerName, Guid branchId, string br
     public string CustomerName { get; private set; } = customerName;
     public Guid BranchId { get; private set; } = branchId;
     public string BranchName { get; private set; } = branchName;
-    public decimal TotalAmount =>  Items
+    public decimal TotalAmount => Items
         .Where(item => !item.IsCanceled)
         .Sum(item => item.TotalAmount);
 
